85904fc722940a0525c1143e858aabae4d2f028f
mansourbash <mansour@zenhr.com>
2023-10-21 18:54:18 +0300

    create full example how to use monorepo architecture with multiple react apps and using common components

---
+++ apps/common/src/components/Hello.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Hello.module.scss'
+const Hello = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>  Welcome from Hello Component</h1>
+      </div>
+  )
+}
+
+export default Hello
